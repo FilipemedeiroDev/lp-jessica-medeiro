5a47e7a09dbd1f9e734d6da9947c5f8af2d2cb83
Filipe <medeirolp@gmail.com>
2024-11-11 14:28:10 -0300

    move imagens para a pasta images e volta a config do astro para o padrão

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel({
-    imageService: true,
-  }),
-});
+export default defineConfig({});
